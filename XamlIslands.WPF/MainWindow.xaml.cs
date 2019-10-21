70051af7ae161ac1e5c714df04b4c5e4b81edf5c
Johan Lindfors <johan.lindfors@coderox.se>
2019-10-21 12:23:47 +0200

    XAML Islands wpf and winforms

---
+++ XamlIslands.WPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace XamlIslands.WPF
+{
+    public partial class MainWindow : Window
+    {
+        Windows.UI.Xaml.Controls.Button uwpButton;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnChildChanged(object sender, EventArgs e)
+        {
+            if (uwpButton == null)
+            {
+                uwpButton = ((Microsoft.Toolkit.Wpf.UI.XamlHost.WindowsXamlHost)sender).Child as Windows.UI.Xaml.Controls.Button;
+                uwpButton.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
+                uwpButton.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
+                uwpButton.Background = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Red);
+                uwpButton.Click += delegate { uwpButton.Background = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Blue); };
+            }
+        }
+    }
+}
